4e6b005b90bba9990e1fb086fa0e86810a6b1ea7
Xmajk <misahrouda98@seznam.cz>
2023-10-06 08:35:12 +0200

    doc

--- Backend/backend/_decorators/apiAuth.py
+++ Backend/backend/_decorators/apiAuth.py
@@ -1,6 +1,6 @@
 
 
-def check_auth():
+def check_auth(view_func):
     """
     Dekorátor, který zkontroluje jestli v headeru requestu jsou atributy
     - hash
@@ -8,7 +8,10 @@ def check_auth():
     
     
     """
-    pass
+    def wrapper_func(request, *args, **kwargs):
+        return view_func(request, *args, **kwargs)
+
+    return wrapper_func
 
 def check_hash():
     pass
