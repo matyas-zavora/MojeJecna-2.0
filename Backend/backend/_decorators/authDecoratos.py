bf04500a93e52aad6361e09e8ddfbf4acaccd5c0
Xmajk <misahrouda98@seznam.cz>
2023-11-02 20:01:05 +0100

    api

--- Backend/backend/_decorators/authDecoratos.py
+++ Backend/backend/_decorators/authDecoratos.py
@@ -1,10 +1,41 @@
+from json import JSONEncoder
+from typing import Any, Union
 from django.http import JsonResponse
 from response import Response
 from _methods.hash import sha256_hash
 from dbModels.entities.authentication.user import User
 from dbModels.entities.authentication.mnUserGroup import MnUserGroup
 
+class JSONResponse(JsonResponse):
+    
+    def __init__(self, data: Any, encoder: type[JSONEncoder] = ..., safe: bool = ..., json_dumps_params: dict[str, Any] | None = ..., **kwargs: Any) -> None:
+        super().__init__(data, encoder, safe, json_dumps_params, **kwargs)
+
+
+
 class AuthDecorators:
+    
+    @staticmethod
+    def __check_hash(user_id: int, user_hash: str) -> Union[JsonResponse, None]:
+        if user_id is None:
+            return Response.make_JSONresponse(
+                401,
+                response_code='',
+                atribute='User-Id'
+            )
+        elif user_hash is None:
+            return Response.make_JSONresponse(
+                401,
+                response_code='',
+                atribute='User-Hash'
+            )
+        user: User = User.objects.get(id=user_id)
+        if not user_hash == user.make_auth_hash():
+            return Response.make_JSONresponse(
+                401,
+                response_code='',
+                atribute='User-Hash'
+            )
 
     @staticmethod
     def check_auth():
@@ -71,3 +102,6 @@ class AuthDecorators:
                 return view_func(request, *args, **kwargs)
             return wrapper_func
         return decorator
+    
+    def check_hash_and_permissions(group_codes='*'):
+        pass
